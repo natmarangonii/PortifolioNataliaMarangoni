a2cbe94471e867206096bf4230cf3b37db13cf86
Natalia <nati.mara2009@gmail.com>
2026-09-23 14:42:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// scroll suave dos botões do nav
+document.querySelectorAll('nav a').forEach(a=>{
+  a.addEventListener('click',e=>{
+    e.preventDefault();
+    const id = a.getAttribute('href');
+    document.querySelector(id)?.scrollIntoView({behavior:'smooth'});
+  });
+});
+
+// botão de contato
+document.getElementById('btnEnviar')?.addEventListener('click', e=>{
+  e.preventDefault();
+  alert('mensagem enviada! 🧡');
+});
+
+ 
